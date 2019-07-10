559dec8b74cbf22f4fc72df567726462cee339e1
Áquila Freitas <aquila.freitas@checkout.com>
2019-07-10 15:39:06 +0100

    Initial commit.

---
+++ checkoutcom.php
@@ -0,0 +1,301 @@
+<?php
+/**
+* 2007-2019 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2019 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class checkoutcom extends PaymentModule
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'checkoutcom';
+        $this->tab = 'payments_gateways';
+        $this->version = '1.0.0';
+        $this->author = 'Checkout.com';
+        $this->need_instance = 1;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Payment Gateway');
+        $this->description = $this->l('Checkout.com is an international provider of online payment solutions. We support 150+ currencies and access to all international cards and popular local payment methods.');
+
+        $this->confirmUninstall = $this->l('');
+
+        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        if (extension_loaded('curl') == false)
+        {
+            $this->_errors[] = $this->l('You have to enable the cURL extension on your server to install this module');
+            return false;
+        }
+
+
+        Configuration::updateValue('CHECKOUTCOM_LIVE_MODE', false);
+
+        include(dirname(__FILE__).'/sql/install.php');
+
+        return parent::install() &&
+            $this->registerHook('header') &&
+            $this->registerHook('backOfficeHeader') &&
+            $this->registerHook('payment') &&
+            $this->registerHook('paymentReturn') &&
+            $this->registerHook('actionPaymentCCAdd') &&
+            $this->registerHook('actionPaymentConfirmation') &&
+            $this->registerHook('displayPayment') &&
+            $this->registerHook('displayPaymentReturn') &&
+            $this->registerHook('displayPaymentTop');
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('CHECKOUTCOM_LIVE_MODE');
+
+        include(dirname(__FILE__).'/sql/uninstall.php');
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitCheckoutcomModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitCheckoutcomModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'CHECKOUTCOM_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter a valid email address'),
+                        'name' => 'CHECKOUTCOM_ACCOUNT_EMAIL',
+                        'label' => $this->l('Email'),
+                    ),
+                    array(
+                        'type' => 'password',
+                        'name' => 'CHECKOUTCOM_ACCOUNT_PASSWORD',
+                        'label' => $this->l('Password'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'CHECKOUTCOM_LIVE_MODE' => Configuration::get('CHECKOUTCOM_LIVE_MODE', true),
+            'CHECKOUTCOM_ACCOUNT_EMAIL' => Configuration::get('CHECKOUTCOM_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'CHECKOUTCOM_ACCOUNT_PASSWORD' => Configuration::get('CHECKOUTCOM_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+    }
+
+    /**
+     * This method is used to render the payment button,
+     * Take care if the button should be displayed or not.
+     */
+    public function hookPayment($params)
+    {
+        $currency_id = $params['cart']->id_currency;
+        $currency = new Currency((int)$currency_id);
+
+
+        $this->smarty->assign('module_dir', $this->_path);
+
+        return $this->display(__FILE__, 'views/templates/hook/payment.tpl');
+    }
+
+    /**
+     * This hook is used to display the order confirmation page.
+     */
+    public function hookPaymentReturn($params)
+    {
+        if ($this->active == false)
+            return;
+
+        $order = $params['objOrder'];
+
+        if ($order->getCurrentOrderState()->id != Configuration::get('PS_OS_ERROR'))
+            $this->smarty->assign('status', 'ok');
+
+        $this->smarty->assign(array(
+            'id_order' => $order->id,
+            'reference' => $order->reference,
+            'params' => $params,
+            'total' => Tools::displayPrice($params['total_to_pay'], $params['currencyObj'], false),
+        ));
+
+        return $this->display(__FILE__, 'views/templates/hook/confirmation.tpl');
+    }
+
+    public function hookActionPaymentCCAdd()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookActionPaymentConfirmation()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookDisplayPayment()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookDisplayPaymentReturn()
+    {
+        /* Place your code here. */
+    }
+
+    public function hookDisplayPaymentTop()
+    {
+        /* Place your code here. */
+    }
+}
---
+++ views/templates/admin/template_1.tpl
@@ -0,0 +1,94 @@
+{*
+* 2007-2019 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2019 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*}
+
+<div class="panel">
+	<div class="row checkoutcom-header">
+		<img src="{$module_dir|escape:'html':'UTF-8'}views/img/template_1_logo.png" class="col-xs-6 col-md-4 text-center" id="payment-logo" />
+		<div class="col-xs-6 col-md-4 text-center">
+			<h4>{l s='Online payment processing' mod='checkoutcom'}</h4>
+			<h4>{l s='Fast - Secure - Reliable' mod='checkoutcom'}</h4>
+		</div>
+		<div class="col-xs-12 col-md-4 text-center">
+			<a href="#" onclick="javascript:return false;" class="btn btn-primary" id="create-account-btn">{l s='Create an account now!' mod='checkoutcom'}</a><br />
+			{l s='Already have an account?' mod='checkoutcom'}<a href="#" onclick="javascript:return false;"> {l s='Log in' mod='checkoutcom'}</a>
+		</div>
+	</div>
+
+	<hr />
+
+	<div class="checkoutcom-content">
+		<div class="row">
+			<div class="col-md-6">
+				<h5>{l s='My payment module offers the following benefits' mod='checkoutcom'}</h5>
+				<dl>
+					<dt>&middot; {l s='Increase customer payment options' mod='checkoutcom'}</dt>
+					<dd>{l s='Visa®, Mastercard®, Diners Club®, American Express®, Discover®, Network and CJB®, plus debit, gift cards and more.' mod='checkoutcom'}</dd>
+
+					<dt>&middot; {l s='Help to improve cash flow' mod='checkoutcom'}</dt>
+					<dd>{l s='Receive funds quickly from the bank of your choice.' mod='checkoutcom'}</dd>
+
+					<dt>&middot; {l s='Enhanced security' mod='checkoutcom'}</dt>
+					<dd>{l s='Multiple firewalls, encryption protocols and fraud protection.' mod='checkoutcom'}</dd>
+
+					<dt>&middot; {l s='One-source solution' mod='checkoutcom'}</dt>
+					<dd>{l s='Conveniance of one invoice, one set of reports and one 24/7 customer service contact.' mod='checkoutcom'}</dd>
+				</dl>
+			</div>
+
+			<div class="col-md-6">
+				<h5>{l s='FREE My Payment Module Glocal Gateway (Value of 400$)' mod='checkoutcom'}</h5>
+				<ul>
+					<li>{l s='Simple, secure and reliable solution to process online payments' mod='checkoutcom'}</li>
+					<li>{l s='Virtual terminal' mod='checkoutcom'}</li>
+					<li>{l s='Reccuring billing' mod='checkoutcom'}</li>
+					<li>{l s='24/7/365 customer support' mod='checkoutcom'}</li>
+					<li>{l s='Ability to perform full or patial refunds' mod='checkoutcom'}</li>
+				</ul>
+				<br />
+				<em class="text-muted small">
+					* {l s='New merchant account required and subject to credit card approval.' mod='checkoutcom'}
+					{l s='The free My Payment Module Global Gateway will be accessed through log in information provided via email within 48 hours.' mod='checkoutcom'}
+					{l s='Monthly fees for My Payment Module Global Gateway will apply.' mod='checkoutcom'}
+				</em>
+			</div>
+		</div>
+
+		<hr />
+
+		<div class="row">
+			<div class="col-md-12">
+				<h4>{l s='Accept payments in the United States using all major credit cards' mod='checkoutcom'}</h4>
+
+				<div class="row">
+					<img src="{$module_dir|escape:'html':'UTF-8'}views/img/template_1_cards.png" class="col-md-6" id="payment-logo" />
+					<div class="col-md-6">
+						<h6 class="text-branded">{l s='For transactions in US Dollars (USD) only' mod='checkoutcom'}</h6>
+						<p class="text-branded">{l s='Call 888-888-1234 if you have any questions or need more information!' mod='checkoutcom'}</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
