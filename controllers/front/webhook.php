38723941098a626433144ada67f3bf15485572c3
Áquila Freitas <aquilacf@protonmail.com>
2019-10-29 11:41:52 +0000

    Debugs.

--- controllers/front/webhook.php
+++ controllers/front/webhook.php
@@ -23,7 +23,7 @@ class CheckoutcomWebhookModuleFrontController extends ModuleFrontController
      */
     public function run()
     {
-
+Debug::write('Webhook.run()');
        $post = file_get_contents("php://input");
         if(Utilities::getValueFromArray($_SERVER, 'HTTP_CKO_SIGNATURE', '') !== hash_hmac('sha256', $post, Configuration::get('CHECKOUTCOM_SECRET_KEY'))) {
 			Debug::write('Invalid Webhook.');
@@ -33,6 +33,7 @@ class CheckoutcomWebhookModuleFrontController extends ModuleFrontController
         $data = null;
         parse_str($post, $data);
         if($data) {
+Debug::write($data);
         	foreach ($data as $key => $value) {
 	        	$this->events []= json_decode($key, true);
 	        }
@@ -47,18 +48,21 @@ class CheckoutcomWebhookModuleFrontController extends ModuleFrontController
      */
     public function handleOrder()
     {
-
+Debug::write('Webhook.handleOrder()');
+Debug::write($this->events);
     	foreach ($this->events as $event) {
 
     		$orders = Order::getByReference($event['data']['reference']);
             $list = $orders->getAll();
-
+Debug::write($list);
+Debug::write($event['data']['reference']);
             $status = Configuration::get('CHECKOUTCOM_FLAGGED_ORDER_STATUS');
             if(isset(static::ACTIONS[$event['type']])) {
                 $status = Configuration::get(static::ACTIONS[$event['type']]);
             }
 
             foreach ($list as $order) {
+Debug::write($order);
                 $history = new OrderHistory();
                 $history->id_order = $order->id;
                 $history->changeIdOrderState($status, $order->id);
