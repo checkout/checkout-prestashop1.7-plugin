10bc7b54b3c46242ff4a28ae2d9deb80dfc6aebb
Áquila Freitas <aquilacf@protonmail.com>
2019-12-11 11:46:33 +0000

    Clear dev branch.

--- checkoutapipayment/skin/js/jquery-v1.front.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-   if($('.cvv-what-is-this').length){
-
-       $('.cvv-what-is-this').click(function(event) {
-           event.preventDefault();
-           $('.tool-tip-content').toggle();
-       })
-   }
-});
