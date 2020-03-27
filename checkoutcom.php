be1d51a88fceb0a916bcfbe250f2de2fc3b63c0f
avish-bisbeehurry-cko <avish.bisbeehurry@checkout.com>
2020-03-27 16:51:54 +0400

    Send integration data in udf5

--- checkoutcom.php
+++ checkoutcom.php
@@ -42,7 +42,7 @@ class CheckoutCom extends PaymentModule
     {
         $this->name = 'checkoutcom';
         $this->tab = 'payments_gateways';
-        $this->version = '2.0.1';
+        $this->version = '2.0.2';
         $this->author = 'Checkout.com';
         $this->need_instance = 1;
 
