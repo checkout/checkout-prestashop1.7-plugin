5b75fed723f8851952ae1c2a76c6e3357cbcaf68
Maxime Galabert <mgalabert@evolutive-business.com>
2022-04-20 13:40:01 +0200

    Update to version 2.2.2

--- checkoutcom.php
+++ checkoutcom.php
@@ -47,7 +47,7 @@ class CheckoutCom extends PaymentModule
     {
         $this->name = 'checkoutcom';
         $this->tab = 'payments_gateways';
-        $this->version = '2.2.1';
+        $this->version = '2.2.2';
         $this->author = 'Checkout.com';
         $this->need_instance = 1;
 
